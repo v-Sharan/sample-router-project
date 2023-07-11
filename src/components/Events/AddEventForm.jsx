91d86aa07e0cebd8c6baa5f6dfc0b05ed9f26113
v-Sharan <vsharansago@gmail.com>
2023-07-11 10:32:38 +0530

    form

---
+++ src/components/Events/AddEventForm.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import classes from "../../styles/form.module.css";
+
+const AddEventForm = () => {
+  return <form></form>;
+};
+
+export default AddEventForm;
--- src/components/Events/EventList.jsx
+++ src/components/Events/EventList.jsx
@@ -2,17 +2,28 @@ import React, { useEffect, useState } from "react";
 
 import classes from "../../styles/EventList.module.css";
 import EventItem from "./EventItem";
+import axios from "axios";
 
 import { getAllEvents } from "../../dummyData";
 
 const EventList = () => {
-  const events = getAllEvents();
+  // const events = getAllEvents();
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    const fectData = async () => {
+      const res = await axios.get(
+        "https://moviedatabase-c8855-default-rtdb.asia-southeast1.firebasedatabase.app/event.json"
+      );
+      console.log(res);
+    };
+    fectData();
+  }, []); //-> data fetching
 
   return (
     <ul className={classes.list}>
-      {events.map((event) => (
-        <EventItem event={event} key={`${event.id}`} />
-      ))}
+      {events.length > 0 &&
+        events.map((event) => <EventItem event={event} key={`${event.id}`} />)}
     </ul>
   );
 };
